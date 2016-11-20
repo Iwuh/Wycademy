a8152e2ea356e26ffd59ceb6769edac79c33d56b
Matthew Faigan <matthew.faigan@gmail.com>
2016-11-20 11:12:42 -0500

    Add basic program

--- WycademyV2/src/WycademyV2/Program.cs
+++ WycademyV2/src/WycademyV2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +9,58 @@ namespace WycademyV2
 {
     public class Program
     {
-        public static void Main(string[] args)
+        // Convert synchronous static Main to an async non-static main method.
+        public static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task Start()
         {
+            // Initialize the DiscordSocketClient and set the LogLevel.
+            _client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            // Hook up a method that prints all logs to the console.
+            _client.Log += Log;
+
+            _client.MessageReceived += async message =>
+            {
+                await Log(new LogMessage(LogSeverity.Info, message.Author.Id == _client.CurrentUser.Id ? ">>Message" : "<<Message", GetUserLogMessage(message)));
+            };
+
+            // Set token to either that of Wycademy or Wycademy Beta depending on whether or not the BETA flag is defined in the build options.
+            string token;
+#if BETA
+            token = Environment.GetEnvironmentVariable("WYCADEMY_BETA_TOKEN");
+#else
+            token = Environment.GetEnvironmentVariable("WYCADEMY_TOKEN");
+#endif
+
+            // Log in and connect.
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.ConnectAsync();
+
+            // Asynchronously block until the bot is exited.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            // Represents a completed Task for methods that have to return Task but don't do any asynchronous work.
+            return Task.CompletedTask;
+        }
+
+        private string GetUserLogMessage(SocketMessage msg)
+        {
+            // If channel is null, then it's a DM message. Otherwise it's a Guild message.
+            var channel = msg.Channel as SocketGuildChannel;
+
+            bool isPrivate = channel == null;
+
+            return $"{(isPrivate ? "Private" : channel.Guild.Name)}{(isPrivate ? "" : "/#" + channel.Name)} from {msg.Author}: {msg.Content}";
         }
     }
 }
