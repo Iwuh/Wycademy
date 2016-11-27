10aaeca17213a56b7afa0e8e70ee48108010a3d9
Matthew Faigan <matthew.faigan@gmail.com>
2016-11-27 16:35:22 -0500

    create custom attribute for checking if locked

---
+++ WycademyV2/src/WycademyV2/Commands/Preconditions/RequireUnlockedAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WycademyV2.Commands.Services;
+
+namespace WycademyV2.Commands.Preconditions
+{
+    public class RequireUnlockedAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissions(CommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            var locker = map.Get<LockerService>();
+
+            if (!locker.IsLocked)
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+            return Task.FromResult(PreconditionResult.FromError("Commands are currently locked."));
+        }
+    }
+}
