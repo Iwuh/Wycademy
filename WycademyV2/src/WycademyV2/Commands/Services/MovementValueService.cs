7a62c81a230754d5ed53e0a088a3ed3f87795e79
Matthew Faigan <matthew.faigan@gmail.com>
2016-11-27 17:20:22 -0500

    Create MovementValueService

---
+++ WycademyV2/src/WycademyV2/Commands/Services/MovementValueService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WycademyV2.Commands.Services
+{
+    /// <summary>
+    /// Provides the movement values for weapons.
+    /// </summary>
+    public class MovementValueService
+    {
+        /// <summary>
+        /// Used for statistics.
+        /// </summary>
+        public int Queries { get; private set; }
+
+        /// <summary>
+        /// Get the file containing the movement values for a specific weapon.
+        /// </summary>
+        /// <param name="name">The name of the weapon to search for.</param>
+        /// <returns>Task(FileStream)</returns>
+        public async Task<FileStream> GetMovementValueStream(string name)
+        {
+
+        }
+    }
+}
