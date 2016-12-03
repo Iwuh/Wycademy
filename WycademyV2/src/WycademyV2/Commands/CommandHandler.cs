36e5aba9a5dd2e2eff7a56e36d6ad5a9721ff7fd
Matthew Faigan <matthew.faigan@gmail.com>
2016-12-03 16:24:39 -0500

    added a comment (ooh)

--- WycademyV2/src/WycademyV2/Commands/CommandHandler.cs
+++ WycademyV2/src/WycademyV2/Commands/CommandHandler.cs
@@ -41,6 +41,7 @@ namespace WycademyV2.Commands
             // If userMessage is null, then it's a system message that should just be ignored.
             var userMessage = msg as SocketUserMessage;
             if (userMessage == null) return;
+            // Ignore any bot messages.
             if (userMessage.Author.IsBot) return;
 
             // The character index to start parsing the command at.
