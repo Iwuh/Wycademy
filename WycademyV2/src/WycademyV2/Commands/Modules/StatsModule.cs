39d1c54ff4d5a2cd41f95f13b8096d5f993716f4
Matthew Faigan <matthew.faigan@gmail.com>
2016-12-04 16:22:16 -0500

    add about command

--- WycademyV2/src/WycademyV2/Commands/Modules/StatsModule.cs
+++ WycademyV2/src/WycademyV2/Commands/Modules/StatsModule.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using WycademyV2.Commands.Preconditions;
@@ -54,12 +55,41 @@ namespace WycademyV2.Commands.Modules
 
                 .AddField(x => x.WithName("Memory Use:").WithValue((p.PrivateMemorySize64 / 1024.0f / 1024.0f).ToString() + " MB").WithIsInline(true))
 
-                .AddField(x => x.WithName("Total Users:").WithValue((Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Users.Count).ToString()))
+                .AddField(x => x.WithName("Total Users:").WithValue((Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Users.Count).ToString()).WithIsInline(true))
 
                 .WithFooter(new EmbedFooterBuilder() { Text = DateTime.Now.ToUniversalTime().ToString("R") });
             }
 
             await Context.Channel.SendCachedMessageAsync(Context.Message.Id, _cache, embed: statsEmbed);
+        }
+
+        [Command("about")]
+        [Summary("Gets information about the bot.")]
+        public async Task GetInfo()
+        {
+            var appInfo = await Context.Client.GetApplicationInfoAsync();
+
+            EmbedBuilder aboutEmbed = new EmbedBuilder()
+                .WithAuthor(new EmbedAuthorBuilder() { Name = "Wycademy", IconUrl = "https://discordapp.com/api/users/207172354101608448/avatars/67bb079bde2e9ed142ad824e4a31d5af.jpg", Url = @"https://github.com/Iwuh/Wycademy" })
+
+                .WithColor(new Color((byte)_rand.GetRandomNumber(0, 256), (byte)_rand.GetRandomNumber(0, 256), (byte)_rand.GetRandomNumber(0, 256)))
+
+                .WithTitle("About the Wycademy")
+
+                .WithDescription("Wycademy is a Monster Hunter info bot, to help you with your hunting without you needing to leave Discord! Check out what it can do by using the `<help` command." +
+                " You can invite it to your server [here](https://discordapp.com/oauth2/authorize?client_id=207172340809859072&scope=bot&permissions=52224) and view the source code [here](https://github.com/Iwuh/Wycademy)." +
+                " If you enjoy the bot, a star would be much appreciated! The icon was created by [@thechewer on Instagram](https://www.instagram.com/p/BH42dbZjiYt/?taken-by=thechewer) and the data is taken from Kiranico." +
+                " Special thanks to my brother for helping me gather the monster data. Monster Hunter is © CAPCOM.")
+
+                .AddField(x => x.WithName("Author:").WithValue($"{appInfo.Owner} (ID: {appInfo.Owner.Id})").WithIsInline(true))
+
+                .AddField(x => x.WithName("Library:").WithValue($"Discord.Net version {DiscordConfig.Version}").WithIsInline(true))
+
+                .AddField(x => x.WithName("Runtime:").WithValue($"{RuntimeInformation.FrameworkDescription} {RuntimeInformation.OSArchitecture}"))
+
+                .WithFooter(new EmbedFooterBuilder() { Text = DateTime.Now.ToUniversalTime().ToString("R") });
+
+            await Context.Channel.SendCachedMessageAsync(Context.Message.Id, _cache, embed: aboutEmbed);
         }
 
         private string GetUptime()
--- WycademyV2/src/WycademyV2/WycademyConst.cs
+++ WycademyV2/src/WycademyV2/WycademyConst.cs
@@ -10,6 +10,11 @@ namespace WycademyV2
         public const string INVALID_MONSTER_NAME = " is not a valid monster name. Try `<monsterlist` for a list of recognised names.";
         public const string INVALID_WEAPON_NAME = " is not a recognized weapon name. Try `<weaponlist` for a list of names.";
 
-        public static readonly DateTime START_TIME = DateTime.Now;
+        public static readonly DateTime START_TIME;
+
+        static WycademyConst()
+        {
+            START_TIME = DateTime.Now;
+        }
     }
 }
