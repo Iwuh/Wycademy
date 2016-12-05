4beb1c601ee3506b1a44be536c622307d9d78c57
Matthew Faigan <matthew.faigan@gmail.com>
2016-12-04 20:45:48 -0500

    Add shutdown command

--- WycademyV2/src/WycademyV2/Commands/Modules/SettingsModule.cs
+++ WycademyV2/src/WycademyV2/Commands/Modules/SettingsModule.cs
@@ -1,8 +1,10 @@
 ﻿using Discord.Commands;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WycademyV2.Commands.Preconditions;
 using WycademyV2.Commands.Services;
 
 namespace WycademyV2.Commands.Modules
@@ -14,14 +16,17 @@ namespace WycademyV2.Commands.Modules
         public class SystemGroup : ModuleBase
         {
             private LockerService _locker;
+            private CommandCacheService _cache;
 
-            public SystemGroup(LockerService locker)
+            public SystemGroup(LockerService locker, CommandCacheService ccs)
             {
                 _locker = locker;
+                _cache = ccs;
             }
 
             [Command("lock")]
             [Summary("Locks the bot, preventing it from responding to commands. If the bot is already locked, unlocks it.")]
+            [RequireOwner]
             public Task SetLocked()
             {
                 if (_locker.IsLocked)
@@ -34,6 +39,30 @@ namespace WycademyV2.Commands.Modules
                 }
 
                 return Task.CompletedTask;
+            }
+
+            [Command("shutdown")]
+            [Summary("Disconnects and closes the bot.")]
+            [RequireOwner]
+            public async Task Shutdown()
+            {
+                if (!_locker.IsLocked)
+                {
+                    await Context.Channel.SendCachedMessageAsync(Context.Message.Id, _cache, text: "Commands must be locked in order to shut down.", prependZWSP: true);
+                }
+                else
+                {
+                    await ReplyAsync("Shutting down...");
+                    await Task.Delay(1000);
+
+                    var client = Context.Client as DiscordSocketClient;
+
+                    await client.DisconnectAsync();
+                    await client.LogoutAsync();
+                    client.Dispose();
+
+                    Environment.Exit(0);
+                }
             }
         }
     }
