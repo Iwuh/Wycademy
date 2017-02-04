1781fcc8c0d6aea1ba43f23801a947968f0dc032
Matthew Faigan <matthew.faigan@gmail.com>
2017-02-04 11:09:37 -0500

    fix file path bugs

--- WycademyV2/src/WycademyV2/Commands/Services/MotionValueService.cs
+++ WycademyV2/src/WycademyV2/Commands/Services/MotionValueService.cs
@@ -28,7 +28,7 @@ namespace WycademyV2.Commands.Services
         /// <returns>Task(FileStream)</returns>
         public FileStream GetMotionValueStream(string name)
         {
-            string fileName = @".\Data\mv\" + AliasWeaponNames(name) + ".txt";
+            string fileName = @"..\..\..\..\Data\mv\" + AliasWeaponNames(name) + ".txt";
 
             Queries++;
 
