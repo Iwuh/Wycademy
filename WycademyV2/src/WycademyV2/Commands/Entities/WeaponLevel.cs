e151ac5c8359a82174d9de7c14c73e93b38610f3
Matthew Faigan <matthew.faigan@gmail.com>
2017-02-16 17:19:58 -0500

    start level class, create sharpness class

---
+++ WycademyV2/src/WycademyV2/Commands/Entities/WeaponLevel.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WycademyV2.Commands.Entities
+{
+    public class WeaponLevel
+    {
+        /// <summary>
+        /// The level of the weapon that these stats apply to.
+        /// </summary>
+        [JsonProperty("level")]
+        public int Level { get; set; }
+
+        /// <summary>
+        /// The raw damage of the level.
+        /// </summary>
+        [JsonProperty("attack")]
+        public int RawDamage { get; set; }
+
+        /// <summary>
+        /// The defense boost that the level grants.
+        /// </summary>
+        [JsonProperty("defense")]
+        public int DefenseBoost { get; set; }
+
+        /// <summary>
+        /// The affinity of the current level.
+        /// </summary>
+        [JsonProperty("affinity")]
+        public int Affinity { get; set; }
+
+        /// <summary>
+        /// How many decoration slots the level has.
+        /// </summary>
+        [JsonProperty("slots")]
+        public int Slots { get; set; }
+
+        /// <summary>
+        /// How many zenny it costs to upgrade or make the level.
+        /// </summary>
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        /// <summary>
+        /// The different sharpness amounts for the current level.
+        /// </summary>
+        [JsonProperty("sharpness")]
+        public List<WeaponSharpness> Sharpness { get; set; }
+    }
+}
---
+++ WycademyV2/src/WycademyV2/Commands/Entities/WeaponSharpness.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WycademyV2.Commands.Enums;
+
+namespace WycademyV2.Commands.Entities
+{
+    public class WeaponSharpness
+    {
+        private Dictionary<SharpnessType, int> _values;
+
+        [JsonConstructor]
+        public WeaponSharpness(int red, int orange, int yellow, int green, int blue, int white)
+        {
+            _values = new Dictionary<SharpnessType, int>()
+            {
+                { SharpnessType.Red, red },
+                { SharpnessType.Orange, orange },
+                { SharpnessType.Yellow, yellow },
+                { SharpnessType.Green, green },
+                { SharpnessType.Blue, blue },
+                { SharpnessType.White, white }
+            };
+        }
+
+        /// <summary>
+        /// Gets the highest sharpness.
+        /// </summary>
+        /// <returns>A string representation of the sharpness colour.</returns>
+        public string GetHighestSharpnessColour()
+        {
+            var ordered = _values.OrderByDescending(p => (int)p.Key);
+            var highest = ordered.FirstOrDefault(p => p.Value > 0);
+
+            return highest.Key.ToString();
+        }
+    }
+}
