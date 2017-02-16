055efe48030113b5b9f03b1d8d6a720ce46f320f
Matthew Faigan <matthew.faigan@gmail.com>
2017-02-15 21:03:32 -0500

    create weapon string class, add documentation.

---
+++ WycademyV2/src/WycademyV2/Commands/Entities/WeaponString.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WycademyV2.Commands.Entities
+{
+    public class WeaponString
+    {
+        /// <summary>
+        /// The in-progress name of the weapon.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The in-progress description of the weapon.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The fully-upgraded name of the weapon.
+        /// </summary>
+        [JsonProperty("max_name")]
+        public string FinalName { get; set; }
+
+        /// <summary>
+        /// The fully-upgraded description of the weapon.
+        /// </summary>
+        [JsonProperty("max_description")]
+        public string FinalDescription { get; set; }
+
+        /// <summary>
+        /// The language of the names and descriptions.
+        /// </summary>
+        [JsonProperty("loc")]
+        public string Language { get; set; }
+    }
+}
