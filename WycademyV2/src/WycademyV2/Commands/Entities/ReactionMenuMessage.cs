6f4b7b0988dcea8a3dd71eaad5ef4b6da2bf7587
Matthew Faigan <matthew.faigan@gmail.com>
2017-03-08 12:20:37 -0500

    create extensible reaction menu service

---
+++ WycademyV2/src/WycademyV2/Commands/Entities/ReactionMenuMessage.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WycademyV2.Commands.Entities
+{
+    public abstract class ReactionMenuMessage
+    {
+        public IUser User { get; protected set; }
+        public string[] Buttons { get; protected set; }
+
+        /// <summary>
+        /// Initializes the User to accept reactions from. (having this constructor forces any derived class to call it with base).
+        /// </summary>
+        /// <param name="user">The user that sent the command.</param>
+        /// <param name="emojis">All the emojis that should be added to the message as menu buttons.</param>
+        protected ReactionMenuMessage(IUser user, params string[] emojis)
+        {
+            User = user;
+            Buttons = emojis;
+        }
+
+        public abstract Task HandleReaction(SocketReaction reaction);
+
+        public abstract Task<IUserMessage> SendMessage(IMessageChannel channel);
+    }
+}
---
+++ WycademyV2/src/WycademyV2/Commands/Services/ReactionMenuService.cs
@@ -0,0 +1,65 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WycademyV2.Commands.Entities;
+using Discord;
+using System.Threading.Tasks;
+
+namespace WycademyV2.Commands.Services
+{
+    public class ReactionMenuService
+    {
+        private Dictionary<ulong, ReactionMenuMessage> _messages;
+        private DiscordSocketClient _client;
+
+        public ReactionMenuService(DiscordSocketClient client)
+        {
+            _messages = new Dictionary<ulong, ReactionMenuMessage>();
+            _client = client;
+            client.ReactionAdded += OnReactionAdded;
+        }
+
+        public async Task<IUserMessage> SendReactionMenuMessageAsync(IMessageChannel channel, ReactionMenuMessage menu)
+        {
+            // Send the message, using the implementation defined by the reaction menu message.
+            var message = await menu.SendMessage(channel);
+
+            // Add all the emojis listed by the reaction menu message.
+            foreach (string button in menu.Buttons)
+            {
+                await message.AddReactionAsync(button);
+            }
+
+            // Add the message to the cache.
+            _messages.Add(message.Id, menu);
+
+            return message;
+        }
+
+        private async Task OnReactionAdded(Cacheable<IUserMessage, ulong> cachedMessage, ISocketMessageChannel channel, SocketReaction reaction)
+        {
+            var message = await cachedMessage.GetOrDownloadAsync();
+            // If the reaction doesn't have a valid user, ignore it.
+            if (!reaction.User.IsSpecified) return;
+
+            // Make sure the message is actually in the list.
+            if (_messages.TryGetValue(message.Id, out ReactionMenuMessage menu))
+            {
+                // Don't trigger anything when originally adding the reactions.
+                if (reaction.UserId == _client.CurrentUser.Id) return;
+                // Ignore any reactions that aren't by the user that used the command.
+                if (reaction.UserId != menu.User.Id) return;
+
+                await menu.HandleReaction(reaction);
+
+                // Remove the user's reaction if the message is in a guild and the bot has the Manage Messages permission.
+                var guildAuthor = message.Author as SocketGuildUser;
+                if (guildAuthor.GuildPermissions.ManageMessages)
+                {
+                    await message.RemoveReactionAsync(reaction.Emoji, menu.User);
+                }
+            }
+        }
+    }
+}
